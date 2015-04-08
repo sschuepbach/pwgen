005d85715bff13803dfec7d01497d75ff0ba122c
sschuepbach <sebastian.schuepbach@unibas.ch>
2015-04-08 13:05:11 +0200

    Initial commit

---
+++ pwgen.py
@@ -0,0 +1,19 @@
+__author__ = 'Sebastian Schüpbach'
+
+import argparse
+import random
+
+parser = argparse.ArgumentParser(description="Generate a random string")
+parser.add_argument('length', metavar='<int>', type=int, help='Length of string')
+args = parser.parse_args()
+
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü']
+chars.extend([x.upper() for x in chars])
+chars.extend(['§', '°', '+', '"', '*', '%', '&', '/', '(', ')', '=', '|', '@', '#', '¬', '¢', '?', '!', ']', '[', '{',
+                '}', '-', '_', '$', '£', '.', ',', ':', ';', '<', '>', '€'])
+chars.extend([x for x in range(0,10)])
+
+for x in range(args.length):
+    random.seed()
+    i = random.sample({y for y in range(len(chars))}, 1)[0]
+    print(chars[i], sep='', end='')
